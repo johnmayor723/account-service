3f0e6b40ec644bc0ef176644d6a06d2ee6dc1b02
wikiwoo <wikiwoo@wikiwoos-MacBook-Pro.local>
2026-07-11 14:19:39 -0700

    feat(errors): add shared API error classes

---
+++ src/errors/index.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ApiError: require("./api.error"),
+
+    ValidationError: require("./classes/validation.error"),
+
+    AuthenticationError: require("./classes/authentication.error"),
+
+    AuthorizationError: require("./classes/authorization.error"),
+
+    NotFoundError: require("./classes/not-found.error"),
+
+    ConflictError: require("./classes/conflict.error"),
+
+    BusinessRuleError: require("./classes/business-rule.error")
+};
